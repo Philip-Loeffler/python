552864be6d3ef589dce723a7c78a162790389ea9
philip-loeffler <philiploeffler@gmail.com>
2021-03-08 10:46:33 -0500

    range

---
+++ SectionFour/iteratingOverRange.py
@@ -0,0 +1,6 @@
+for i in range(1, 20):
+    print("i is not {}".format(i))
+
+# in ranges the last value specified is not included
+# range produces a rang of numbers - from the starting value, up to but not including the end value
+# so with the range from 1, 20...its only going to print out 1-19

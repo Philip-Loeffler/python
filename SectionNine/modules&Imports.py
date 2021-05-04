685338ba70f6baa134dff88f12818be33eec2d85
philip-loeffler <philiploeffler@gmail.com>
2021-05-04 09:35:25 -0400

    turtle game, imports not working though

---
+++ SectionNine/modules&Imports.py
@@ -0,0 +1,12 @@
+import turtle
+# can use this so we just import the forward method
+# from turtle import forward
+
+# and then we can do this
+# forward(150)
+turtle.forward(150)
+turtle.right(250)
+turtle.forward(150)
+turtle.circle(84)
+
+turtle.done()
